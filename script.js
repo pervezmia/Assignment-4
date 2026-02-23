b296d2af763b6cec104ccb40b1cbcfe0fe3bb114
pervezmia <ahmedpervezkabir@gmail.com>
2026-02-23 17:36:33 +0600

    addEvenListener added of allCardSection

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-let interviewList = [1,2,3,];
-let rejectedList = [1,2,];
+let interviewList = [];
+let rejectedList = [];
+
 
 const currentStatus = "all-filtering-btn";
 
-const totalCount = document.getElementById("total"); 
+const totalCount = document.getElementById("total");
 const availableJobs = document.getElementById("availableJobs");
 const interviewCount = document.getElementById("interviewCount");
 const rejectedCount = document.getElementById("rejectedCount");
@@ -16,10 +17,10 @@ const rejectedFilteringBtn = document.getElementById("rejected-filtering-btn");
 
 const allCardsSection = document.getElementById("all-cards");
 
-// console.log(interviewCount.innerText, rejectedCount.innerText);
+let filterSection = document.getElementById("filter-section");
 
 //calculation
-function calculate (){
+function calculate() {
     totalCount.innerText = allCardsSection.children.length;
     availableJobs.innerText = allCardsSection.children.length;
     interviewCount.innerText = interviewList.length;
@@ -30,20 +31,60 @@ calculate();
 
 
 // toggling
-function togglingStyle (id){
-    
+function togglingStyle(id) {
+
     allFilteringBtn.classList.add("bg-gray-300", "text-black");
     interviewFilteringBtn.classList.add("bg-gray-300", "text-black");
     rejectedFilteringBtn.classList.add("bg-gray-300", "text-black");
-    
+
     allFilteringBtn.classList.remove("bg-amber-300", "text-white");
     interviewFilteringBtn.classList.remove("bg-amber-300", "text-white");
     rejectedFilteringBtn.classList.remove("bg-amber-300", "text-white");
-    
+
     const clickedBtn = document.getElementById(id);
 
     clickedBtn.classList.remove("bg-gray-300", "text-black");
     clickedBtn.classList.add("bg-amber-300", "text-white");
 }
 togglingStyle("all-filtering-btn");
+
+
+//event delegation orthat parent a click kore btn child dhore kaj korsi
+
+allCardsSection.addEventListener("click", function (event) {
+    const successBtn = event.target.closest(".success-btn-of-card");
+    const warningBtn = event.target.closest(".warning-btn-of-card");
+    const deleteBtn = event.target.closest(".delete-btn-of-card");
+
+
+    if (successBtn) {
+        const parentNode = event.target.closest(".card-item")
+
+        const title = parentNode.querySelector(".title-of-card").innerText;
+        const skill = parentNode.querySelector(".skill-of-card").innerText;
+        const salary = parentNode.querySelector(".salary-of-card").innerText;
+        const status = parentNode.querySelector(".status-of-card").innerText;
+        const description = parentNode.querySelector(".description-of-card").innerText;
+
+        const cardInfo = {
+            title,
+            skill,
+            salary,
+            status: "Interview",
+            description
+        }
+
+
+
+        const existInterview = interviewList.find(item => item.title === cardInfo.title);
+
+
+        if (!existInterview) {
+            interviewList.push(cardInfo);
+        }
+
+    }
+})
+
+
 
